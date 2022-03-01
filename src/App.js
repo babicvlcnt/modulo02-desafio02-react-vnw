caddba0b957d1e113af868416e1bbd47559ac3a0
babicvlcnt <bcfrota@id.uff.br>
2022-03-01 15:26:45 -0300

    finalizada

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    n1: "",
+    n2: "",
+    res: ""
+  };
+
+  div = () => {
+    const { n1, n2 } = this.state;
+    this.setState({
+      res: n1 / n2
+    });
+  };
+
+  mult = () => {
+    const { n1, n2 } = this.state;
+    this.setState({
+      res: n1 * n2
+    });
+  };
+
+  subt = () => {
+    const { n1, n2 } = this.state;
+    this.setState({
+      res: n1 - n2
+    });
+  };
+
+  adc = () => {
+    const { n1, n2 } = this.state;
+    this.setState({
+      res: parseInt(n1) + parseInt(n2)
+    });
+  };
+
+  clear = () => {
+    const { n1, n2 } = this.state;
+    this.setState({
+      res: 0
+    });
+  };
+
+  handleChange2 = (event) => {
+    this.setState({
+      n2: event.target.value
+    });
+  };
+
+  handleChange1 = (event) => {
+    this.setState({
+      n1: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="contain-calculator">
+        <h1>Calculadora</h1>
+        <input onChange={this.handleChange1} />
+        <input onChange={this.handleChange2} />
+        <div className="contain-buttons">
+          <button onClick={this.div}>/</button>
+          <button onClick={this.mult}>*</button>
+          <button onClick={this.subt}>-</button>
+          <button onClick={this.adc}>+</button>
+          <button onClick={this.clear}>C</button>
+        </div>
+        <h2>{this.state.res}</h2>
+      </div>
+    );
+  }
 }
-
 export default App;
